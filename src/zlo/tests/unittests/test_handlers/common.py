674da456097efc374de9a163bfdb581ec9c88080
andrsj <andrsj.derkach@gmail.com>
2020-11-02 18:39:12 +0200

    get -> given

--- src/zlo/tests/unittests/test_handlers/common.py
+++ src/zlo/tests/unittests/test_handlers/common.py
@@ -8,7 +8,7 @@ from zlo.tests.fixture import prepare_game, generate_ten_slots_for_game
 
 class BaseTestHadnler:
 
-    def get_prepare_game_and_houses(self):
+    def given_prepare_game_and_houses(self):
         self._uown = FakeUnitOfWorkManager()
         self.game = prepare_game(uow=self._uown)
         self.houses: List[House] = generate_ten_slots_for_game(self.game.game_id)

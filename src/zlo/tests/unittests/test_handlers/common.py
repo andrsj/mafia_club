0d4727e1e6e5a9726f17f64585f156df0b6a60b9
andrsj <andrsj.derkach@gmail.com>
2020-11-02 11:52:19 +0200

    Add BaseTestClass

---
+++ src/zlo/tests/unittests/test_handlers/common.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+from zlo.domain.model import House
+from zlo.tests.fakes import FakeUnitOfWorkManager
+from zlo.tests.fixture import prepare_game, generate_ten_slots_for_game
+
+
+class BaseTestHadnler:
+
+    def get_prepare_game_and_houses(self):
+        self._uown = FakeUnitOfWorkManager()
+        self.game = prepare_game(uow=self._uown)
+        self.houses: List[House] = generate_ten_slots_for_game(self.game.game_id)
+
+        for house in self.houses:
+            self._uown.sess.houses.add(house)
